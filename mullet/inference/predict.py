4aa39143a7334a3f5c63ab7d47d0ba60393f8e85
shenhai <shenhai1895@hotmail.com>
2023-06-14 16:22:57 +0800

    test

--- mullet/inference/predict.py
+++ mullet/inference/predict.py
@@ -9,22 +9,18 @@ import torch.multiprocessing as mp
 
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import DataLoader
+from mullet.datasets.dataset import MultiPhaseMultiSliceInferenceDataset
+from mullet.models.mullet import MULLET
 from mullet.scripts.post import max_region, vote
-
 from mullet.utils.utils import mkdir, setup, cleanup
-
-# from scripts.post import max_region, vote
-# from datasets.dataset import TestDualPhaseMultiSliceDataset, transforms_dla_test
-# from models.mullet import DualSeg
-# from utils.utils import mkdir, setup, cleanup
 
 
 def inference(rank, world_size, args):
     setup(rank, world_size, port=args.port)
     test_file_list = args.test_file_list[rank]
     num_classes = args.num_classes
-    model = DualSeg(cls=num_classes, num_tokens=16,
-                    n_context=args.n_ctx, bn=torch.nn.SyncBatchNorm).cuda(rank)
+    model = MULLET(cls=num_classes, num_tokens=24,
+                   n_context=args.n_ctx, bn=torch.nn.SyncBatchNorm).cuda(rank)
     model = DDP(model, device_ids=[
                 rank], output_device=rank, find_unused_parameters=True)
     checkpoint = torch.load(os.path.join(
@@ -32,27 +28,31 @@ def inference(rank, world_size, args):
     model.module.load_state_dict(checkpoint['model'])
     model.eval()
     for name in test_file_list:
-        dataset = TestDualPhaseMultiSliceDataset(os.path.join(args.test_dir, name),
-                                                 transforms=transforms_dla_test, n_ctx=args.n_ctx)
+        dataset = MultiPhaseMultiSliceInferenceDataset(
+            os.path.join(args.i, name), n_ctx=args.n_ctx)
         d = DataLoader(dataset, args.batch_size, num_workers=args.num_workers)
         s, z, w, h = dataset.shape
         pred_tumor_mask = torch.zeros((s, z, num_classes, w, h))
         pred_tumor_num = torch.zeros((s, z, num_classes, w, h))
         time_start = time.time()
+        phase_idx = [0, 1, 2, 3]
         for images, images_bag, key_idx, z in d:
             images_bag = images_bag.to(rank)
             with torch.no_grad():
-                mask_1, mask_2 = model(images_bag)  # (b, c, z, h, w)
+                mask_0, mask_1, mask_2, mask_3 = model(images_bag)  # (b, c, z, h, w)
+                mask_0 = torch.softmax(mask_0, 1).permute((0, 2, 1, 3, 4))
                 mask_1 = torch.softmax(mask_1, 1).permute((0, 2, 1, 3, 4))
                 mask_2 = torch.softmax(mask_2, 1).permute((0, 2, 1, 3, 4))
-                for mask_1_i, mask_2_i, z_i in zip(mask_1, mask_2, z):
-                    pred_tumor_mask[0, z_i:z_i + args.n_ctx] += mask_1_i.cpu()
-                    pred_tumor_mask[1, z_i:z_i + args.n_ctx] += mask_2_i.cpu()
-                    pred_tumor_num[:, z_i:z_i + args.n_ctx] += 1
+                mask_3 = torch.softmax(mask_3, 1).permute((0, 2, 1, 3, 4))
+                for mask_0_i, mask_1_i, mask_2_i, mask_3_i, z_i in zip(mask_0, mask_1, mask_2, mask_3, z):
+                    pred_tumor_mask[phase_idx[0], z_i:z_i + args.n_ctx] += mask_0_i.cpu()
+                    pred_tumor_mask[phase_idx[1], z_i:z_i + args.n_ctx] += mask_1_i.cpu()
+                    pred_tumor_mask[phase_idx[2], z_i:z_i + args.n_ctx] += mask_2_i.cpu()
+                    pred_tumor_mask[phase_idx[3], z_i:z_i + args.n_ctx] += mask_3_i.cpu()
+                    pred_tumor_num[phase_idx, z_i:z_i + args.n_ctx] += 1
         s = dataset.base_num
         e = dataset.base_num + dataset.len + args.n_ctx - 1
-        pred_tumor_mask[:, s:e] = pred_tumor_mask[:, s:e] / \
-            pred_tumor_num[:, s:e]
+        pred_tumor_mask[:, s:e] = pred_tumor_mask[:, s:e] / pred_tumor_num[:, s:e]
         pred_tumor_mask[:, :, 0, :, :] += 1e-15
         pred_tumor_mask = torch.argmax(pred_tumor_mask, 2)
         time_end = time.time()
@@ -64,22 +64,39 @@ def inference(rank, world_size, args):
             pred_tumor_mask[i][drop] = 0
             pred_tumor_mask[i] = vote(pred_tumor_mask[i])
 
-        seg_a = pred_tumor_mask[0]
+        mkdir(os.path.join(args.o, name))
+
+        seg_p = pred_tumor_mask[0]
+        savedImg = sitk.GetImageFromArray(seg_p)
+        savedImg.SetSpacing(dataset.spacing)
+        savedImg.SetOrigin(dataset.origin)
+        sitk.WriteImage(savedImg, os.path.join(
+            args.o, name, "segmentation_p.nii.gz"))
+
+        seg_a = pred_tumor_mask[1]
         savedImg = sitk.GetImageFromArray(seg_a)
         savedImg.SetSpacing(dataset.spacing)
         savedImg.SetOrigin(dataset.origin)
         sitk.WriteImage(savedImg, os.path.join(
-            args.test_dir, name, name + "_seg_a_pred.nii.gz"))
-
-        seg_v = pred_tumor_mask[1]
+            args.o, name, "segmentation_a.nii.gz"))
+        
+        seg_v = pred_tumor_mask[2]
         savedImg = sitk.GetImageFromArray(seg_v)
         savedImg.SetSpacing(dataset.spacing)
         savedImg.SetOrigin(dataset.origin)
         sitk.WriteImage(savedImg, os.path.join(
-            args.test_dir, name, name + "_seg_v_pred.nii.gz"))
+            args.o, name, "segmentation_v.nii.gz"))
+        
+        seg_d = pred_tumor_mask[3]
+        savedImg = sitk.GetImageFromArray(seg_d)
+        savedImg.SetSpacing(dataset.spacing)
+        savedImg.SetOrigin(dataset.origin)
+        sitk.WriteImage(savedImg, os.path.join(
+            args.o, name, "segmentation_d.nii.gz"))
+
         print(name, str(time_end - time_start) + "s")
         print("Saved the segmentation in ", os.path.join(
-            args.test_dir, name, name + "_seg_v_pred.nii.gz"))
+            args.o, name))
     cleanup()
 
 
@@ -94,7 +111,7 @@ def run_inference(test_fn, args, world_size):
     Returns:
 
     """
-    test_file_list = [i for i in os.listdir(args.test_dir) if "example_" in i]
+    test_file_list = [i for i in os.listdir(args.i) if "example_" in i]
     test_file_list = sorted(test_file_list)
     test_file_list = np.array_split(test_file_list, world_size)
     args.test_file_list = test_file_list
@@ -106,10 +123,11 @@ def run_inference(test_fn, args, world_size):
 
 def predict_entry_point():
     parser = argparse.ArgumentParser('Multi-phase Liver Lesion Segmentation')
-    parser.add_argument('-i', type=str, required=True)
-    parser.add_argument('-o', type=str, required=True)
+    parser.add_argument('-i', type=str, default="/data0/raw_data/sss/nii/")
+    parser.add_argument('-o', type=str, default="/data0/raw_data/sss/out/")
+    parser.add_argument('--checkpoint_path', type=str, default="/data0/wulei/train_log/p_a_v_d/model_23.pth")
     parser.add_argument('--num_classes', type=int, default=4)
-    parser.add_argument('--batch_size', type=int, default=5)
+    parser.add_argument('--batch_size', type=int, default=1)
     parser.add_argument('--n_ctx', type=int, default=3)
     parser.add_argument('--num_workers', type=int, default=1)
     parser.add_argument('--devices', type=int, nargs='+', default=[0])
@@ -124,23 +142,5 @@ def predict_entry_point():
     run_inference(inference, args, n_gpus)
 
 
-# if __name__ == '__main__':
-#     parser = argparse.ArgumentParser('Multi-phase Liver Lesion Segmentation')
-#     parser.add_argument('--test_dir', type=str, default="/xxx/data")
-#     parser.add_argument('--checkpoint_path', type=str,
-#                         default="/data0/xxx/train_log/model.pth")
-#     parser.add_argument('--num_classes', type=int, default=4)
-#     parser.add_argument('--batch_size', type=int, default=5)
-#     parser.add_argument('--n_ctx', type=int, default=3)
-#     parser.add_argument('--num_workers', type=int, default=1)
-#     parser.add_argument('--same_slice', type=bool, default=False)
-#     parser.add_argument('--devices', type=int, nargs='+', default=[0])
-#     parser.add_argument('--port', type=str, default="1894")
-
-#     args = parser.parse_args()
-
-#     devices = ','.join([str(s) for s in args.devices])
-#     os.environ["CUDA_VISIBLE_DEVICES"] = devices
-
-#     n_gpus = torch.cuda.device_count()
-#     run_test(test, args, n_gpus)
+if __name__ == '__main__':
+    predict_entry_point()
--- mullet/models/mullet.py
+++ mullet/models/mullet.py
@@ -4,6 +4,8 @@ import torch.nn.functional as F
 from segmentation_models_pytorch.encoders import get_encoder
 from torch.nn import TransformerDecoderLayer, LayerNorm, TransformerDecoder
 
+from mullet.nn.positional_encoding import PositionalEncoding1D, PositionalEncodingPermute2D
+
 
 class GlobalZ(nn.Module):
     def __init__(self, in_channel=512):
@@ -14,6 +16,7 @@ class GlobalZ(nn.Module):
         self.key_bn = nn.GroupNorm(1, in_channel // 2)
         self.value = nn.Conv2d(in_channel, in_channel, (1, 1))
         self.value_bn = nn.GroupNorm(1, in_channel)
+        self.pos = PositionalEncoding1D(in_channel // 2)
 
     def forward(self, query, key, value):
         b, n, c, h, w = query.shape
@@ -32,7 +35,10 @@ class GlobalZ(nn.Module):
         query_pool_g = query_pool_g.view(b, n, c // 2)
         key_pool_g = F.adaptive_avg_pool2d(key, (1, 1))
         key_pool_g = key_pool_g.view(b, n, c // 2)
-        sim_slice = torch.einsum('bmd,bnd->bmn', query_pool_g, key_pool_g)
+
+        pos = self.pos(key_pool_g)
+
+        sim_slice = torch.einsum('bmd,bnd->bmn', query_pool_g + pos, key_pool_g + pos)
         sim_slice = sim_slice / (c // 2) ** 0.5
         sim_slice = torch.softmax(sim_slice, dim=-1)
         context_pool_slice = torch.einsum('bmn,bnchw->bmchw', sim_slice, value.reshape(b, n, c, h, w))
@@ -50,16 +56,20 @@ class GlobalS(nn.Module):
         self.value = nn.Conv2d(in_channel, in_channel, (1, 1))
         self.value_bn = nn.GroupNorm(1, in_channel)
 
+        self.pos_emb = PositionalEncodingPermute2D(in_channel // 2)
+
     def forward(self, query, key, value):
         b, n, c, h, w = query.shape
         query = query.reshape(b * n, c, h, w)
         key = key.reshape(b * n, c, h, w)
         value = value.reshape(b * n, c, h, w)
 
+        pos = self.pos_emb(query)
+
         query = self.query(query)
-        query = self.query_bn(query)
+        query = self.query_bn(query) + pos
         key = self.key(key)
-        key = self.key_bn(key)
+        key = self.key_bn(key) + pos
         value = self.value(value)
         value = self.value_bn(value)
 
@@ -78,6 +88,47 @@ class GlobalS(nn.Module):
 
         return context_pool_s
 
+class Decoder(nn.Module):
+    def __init__(self, num_classes, high_level_inplanes, low_level_inplanes, BatchNorm):
+        super(Decoder, self).__init__()
+        self.conv1 = nn.Conv2d(low_level_inplanes, 256, 1, bias=False)
+        self.bn1 = BatchNorm(256)
+        self.relu = nn.Mish(inplace=True)
+        self.last_conv = nn.Sequential(
+            nn.Conv2d(high_level_inplanes + 256, 256, kernel_size=3, stride=1, padding=1, bias=False),
+            BatchNorm(256),
+            nn.Mish(inplace=True),
+            nn.Dropout(0.5),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=False),
+            BatchNorm(256),
+            nn.Mish(inplace=True),
+            nn.Dropout(0.1),
+            nn.Conv2d(256, num_classes, kernel_size=3,
+                      padding=1)
+        )
+        self._init_weight()
+
+    def forward(self, x, low_level_feat):
+        low_level_feat = self.conv1(low_level_feat)
+        low_level_feat = self.bn1(low_level_feat)
+        low_level_feat = self.relu(low_level_feat)
+        x = F.interpolate(x, size=low_level_feat.size()[2:], mode='bilinear', align_corners=True)
+        x = torch.cat((x, low_level_feat), dim=1)
+        x = self.last_conv(x)
+
+        return x
+
+    def _init_weight(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                torch.nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.SyncBatchNorm):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
 
 class TokenLearnerModuleV2(nn.Module):
     def __init__(self, in_channel=512, num_tokens=8):
@@ -86,11 +137,18 @@ class TokenLearnerModuleV2(nn.Module):
         self.ln = nn.GroupNorm(1, in_channel)
         self.global_z = GlobalZ(in_channel)
         self.global_s = GlobalS(in_channel)
-        # self.conv1 = nn.Conv2d(in_channel, in_channel, (1, 1), groups=8, bias=False)
         self.conv_3x3 = nn.Conv3d(in_channel, in_channel, (3, 3, 3), groups=8, bias=False, padding=(1, 1, 1))
-        # self.conv_5x5 = nn.Conv3d(in_channel, in_channel, (5, 5, 5), groups=8, bias=False, padding=(2, 2, 2))
-        # self.conv_7x7 = nn.Conv3d(in_channel, in_channel, (7, 7, 7), groups=8, bias=False, padding=(3, 3, 3))
-        self.conv2 = nn.Conv2d(in_channel, num_tokens, (1, 1), bias=False)
+        self.conv2 = nn.Sequential(nn.Conv2d(in_channel, in_channel, (1, 1), bias=False),
+                                   nn.GroupNorm(1, in_channel),
+                                   nn.Mish(),
+                                   nn.Conv2d(in_channel, in_channel, (1, 1), bias=False),
+                                   nn.GroupNorm(1, in_channel),
+                                   nn.Mish(),
+                                   nn.Conv2d(in_channel, in_channel, (1, 1), bias=False),
+                                   nn.GroupNorm(1, in_channel),
+                                   nn.Mish(),
+                                   nn.Conv2d(in_channel, num_tokens, (1, 1), bias=False),
+                                   )
         self.conv_feat = nn.Conv2d(in_channel, in_channel, (1, 1), groups=8, bias=False)
 
     def forward(self, x, x_aux):
@@ -101,7 +159,7 @@ class TokenLearnerModuleV2(nn.Module):
         local = torch.sigmoid(local * x.permute(0, 2, 1, 3, 4)) * local
         local = local.permute(0, 2, 1, 3, 4)
 
-        selected = global_z + global_s + local + x
+        selected = x + global_z + global_s + local
         selected = self.ln(selected.reshape(b * n, c, h, w))
         feat = selected
         selected = self.conv2(selected)
@@ -123,11 +181,9 @@ class TokenFuser(nn.Module):
         self.mix = nn.Conv2d(in_channel, num_tokens, (1, 1), groups=8, bias=False)
 
     def forward(self, tokens, origin):
-        # tokens = tokens.permute(0, 2, 1)
         tokens = self.ln1(tokens)
         tokens = self.conv1(tokens)
         tokens = self.ln2(tokens)
-        # tokens = tokens.permute(0, 2, 1)
 
         origin = self.ln3(origin)
         origin = self.mix(origin)
@@ -144,100 +200,61 @@ class TransformerBlock(nn.Module):
         self.n_ctx = n_ctx
         self.num_tokens = num_tokens
         self.in_channel = in_channel
-        # self.ln1 = nn.GroupNorm(1, in_channel)
-        # self.ln2 = nn.GroupNorm(1, in_channel)
-        # self.ln3 = nn.GroupNorm(1, in_channel)
-        # self.mha1 = nn.MultiheadAttention(embed_dim=in_channel,
-        #                                   num_heads=8,
-        #                                   dropout=0.1,
-        #                                   bias=False)
         decoder_layer = TransformerDecoderLayer(in_channel, nhead=8, dim_feedforward=1024, dropout=0.1,
                                                 batch_first=True)
         decoder_norm = LayerNorm(in_channel)
+        self.TokenLearner_p = TokenLearnerModuleV2(in_channel, num_tokens=num_tokens)
         self.TokenLearner_a = TokenLearnerModuleV2(in_channel, num_tokens=num_tokens)
         self.TokenLearner_v = TokenLearnerModuleV2(in_channel, num_tokens=num_tokens)
-        self.decoder_a = TransformerDecoder(decoder_layer, 1, decoder_norm)
-        self.decoder_v = TransformerDecoder(decoder_layer, 1, decoder_norm)
+        self.TokenLearner_d = TokenLearnerModuleV2(in_channel, num_tokens=num_tokens)
+        self.decoder_p = TransformerDecoder(decoder_layer, 6, decoder_norm)
+        self.decoder_a = TransformerDecoder(decoder_layer, 6, decoder_norm)
+        self.decoder_v = TransformerDecoder(decoder_layer, 6, decoder_norm)
+        self.decoder_d = TransformerDecoder(decoder_layer, 6, decoder_norm)
         self.TokenFuser = TokenFuser(in_channel, num_tokens=num_tokens)
 
     def forward(self, x):
-        x_a, x_v = x[:, 0], x[:, 1]
+        x_p, x_a, x_v, x_d = x[:, 0], x[:, 1], x[:, 2], x[:, 3]
         b, n, c, h, w = x_a.shape
+        token_p = self.TokenLearner_p(x_p, x_v)
         token_a = self.TokenLearner_a(x_a, x_v)
         token_v = self.TokenLearner_v(x_v, x_a)
+        token_d = self.TokenLearner_d(x_d, x_a)
 
+        token_p = token_p.reshape(b, n * self.num_tokens, c)
         token_a = token_a.reshape(b, n * self.num_tokens, c)
         token_v = token_v.reshape(b, n * self.num_tokens, c)
+        token_d = token_d.reshape(b, n * self.num_tokens, c)
 
-        token_a_tmp = self.decoder_a(token_a, torch.cat([token_a, token_v], 1))
-        token_v_tmp = self.decoder_v(token_v, torch.cat([token_v, token_a], 1))
+        token_p_tmp = self.decoder_p(token_p, torch.cat([token_p, token_a, token_v, token_d], 1))
+        token_a_tmp = self.decoder_a(token_a, torch.cat([token_p, token_a, token_v, token_d], 1))
+        token_v_tmp = self.decoder_v(token_v, torch.cat([token_p, token_a, token_v, token_d], 1))
+        token_d_tmp = self.decoder_d(token_d, torch.cat([token_p, token_a, token_v, token_d], 1))
 
+        token_p_tmp = token_p_tmp.reshape(-1, self.num_tokens, self.in_channel).permute(0, 2, 1)
         token_a_tmp = token_a_tmp.reshape(-1, self.num_tokens, self.in_channel).permute(0, 2, 1)
         token_v_tmp = token_v_tmp.reshape(-1, self.num_tokens, self.in_channel).permute(0, 2, 1)
+        token_d_tmp = token_d_tmp.reshape(-1, self.num_tokens, self.in_channel).permute(0, 2, 1)
 
+        x_p = x_p.reshape(b * n, c, h, w)
         x_a = x_a.reshape(b * n, c, h, w)
         x_v = x_v.reshape(b * n, c, h, w)
+        x_d = x_d.reshape(b * n, c, h, w)
+        x_p = self.TokenFuser(token_p_tmp, x_p) + x_p
         x_a = self.TokenFuser(token_a_tmp, x_a) + x_a
         x_v = self.TokenFuser(token_v_tmp, x_v) + x_v
-        # x_a = self.ln2(x_a)
-        # x_v = self.ln3(x_v)
-        out = torch.stack([x_a, x_v], 1).reshape(b, -1, n, c, h, w)
+        x_d = self.TokenFuser(token_d_tmp, x_d) + x_d
+        out = torch.stack([x_p, x_a, x_v, x_d], 1).reshape(b, -1, n, c, h, w)
         return out
 
 
-class Decoder(nn.Module):
-    def __init__(self, num_classes, high_level_inplanes, low_level_inplanes, BatchNorm):
-        super(Decoder, self).__init__()
-        self.conv1 = nn.Conv2d(low_level_inplanes, 256, 1, bias=False)
-        self.bn1 = BatchNorm(256)
-        self.relu = nn.ReLU(inplace=True)
-        self.last_conv = nn.Sequential(
-            nn.Conv2d(high_level_inplanes + 256, 256, kernel_size=3, stride=1, padding=1, bias=False),
-            BatchNorm(256),
-            nn.ReLU(inplace=True),
-            nn.Dropout(0.5),
-            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=False),
-            BatchNorm(256),
-            nn.ReLU(inplace=True),
-            nn.Dropout(0.1),
-            nn.Conv2d(256, num_classes, kernel_size=3,
-                      padding=1)
-            )
-        self._init_weight()
-
-    def forward(self, x, low_level_feat):
-        low_level_feat = self.conv1(low_level_feat)
-        low_level_feat = self.bn1(low_level_feat)
-        low_level_feat = self.relu(low_level_feat)
-        # print(low_level_feat.size())
-        # print(x.size())
-        # x = self._modules['upsampling%dx' % 2**self.n_upsamples](x)
-        x = F.interpolate(x, size=low_level_feat.size()[2:], mode='bilinear', align_corners=True)
-        x = torch.cat((x, low_level_feat), dim=1)
-        # x = F.interpolate(x, scale_factor=4, mode='bilinear', align_corners=True)
-        x = self.last_conv(x)
-
-        return x
-
-    def _init_weight(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                torch.nn.init.kaiming_normal_(m.weight)
-            elif isinstance(m, nn.SyncBatchNorm):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-
-
-class DualSeg(nn.Module):
+class MULLET(nn.Module):
     def __init__(self, encoder_name="resnet34",
                  in_channels=3,
                  encoder_depth=5,
-                 num_tokens=8,
+                 num_tokens=24,
                  cls=4,
-                 n_context=9,
+                 n_context=3,
                  bn=nn.BatchNorm2d
                  ):
         super().__init__()
@@ -258,69 +275,40 @@ class DualSeg(nn.Module):
     def forward(self, x):
         b, s, n, c, h, w = x.shape
         x = x.reshape(b * s * n, c, h, w)
-        # x_v = x.reshape(b * s * n, c, h, w)
         x_feat = self.encoder(x)
-        # x_v_feat = self.encoder(x_v)
         h_feat, low_feat = x_feat[-1], x_feat[2]
         h_feat = h_feat.reshape(b, s, n, *h_feat.shape[1:])
         low_feat = low_feat.reshape(b, s, n, *low_feat.shape[1:])
         h_feat = self.TransformerBlock(h_feat)
-        # h_feat = self.TransformerBlock_2(h_feat)
-        feat_a = h_feat[:, 0].reshape(-1, *h_feat.shape[3:])
-        feat_v = h_feat[:, 1].reshape(-1, *h_feat.shape[3:])
-        x_a_masks = self.decoder(feat_a, low_feat[:, 0].reshape(-1, *low_feat.shape[3:]))
-        x_v_masks = self.decoder(feat_v, low_feat[:, 1].reshape(-1, *low_feat.shape[3:]))
-        # x_a_feat[-2], x_v_feat[-2] = self.TransformerBlock_2(x_a_feat[-2], x_v_feat[-2])
-        # x_a_output = self.decoder(*x_a_feat)
-        # x_v_output = self.decoder(*x_v_feat)
-        # x_a_masks = self.segmentation_head(x_a_output)
-        # x_v_masks = self.segmentation_head(x_v_output)
+
+        feat_p = h_feat[:, 0].reshape(-1, *h_feat.shape[3:])
+        feat_a = h_feat[:, 1].reshape(-1, *h_feat.shape[3:])
+        feat_v = h_feat[:, 2].reshape(-1, *h_feat.shape[3:])
+        feat_d = h_feat[:, 3].reshape(-1, *h_feat.shape[3:])
+
+        x_p_masks = self.decoder(feat_p, low_feat[:, 0].reshape(-1, *low_feat.shape[3:]))
+        x_a_masks = self.decoder(feat_a, low_feat[:, 1].reshape(-1, *low_feat.shape[3:]))
+        x_v_masks = self.decoder(feat_v, low_feat[:, 2].reshape(-1, *low_feat.shape[3:]))
+        x_d_masks = self.decoder(feat_d, low_feat[:, 3].reshape(-1, *low_feat.shape[3:]))
+
+        x_p_masks = F.interpolate(x_p_masks, size=(h, w), mode='bilinear', align_corners=True)
         x_a_masks = F.interpolate(x_a_masks, size=(h, w), mode='bilinear', align_corners=True)
         x_v_masks = F.interpolate(x_v_masks, size=(h, w), mode='bilinear', align_corners=True)
+        x_d_masks = F.interpolate(x_d_masks, size=(h, w), mode='bilinear', align_corners=True)
+
+        x_p_masks = x_p_masks.reshape(b, n, -1, h, w).permute(0, 2, 1, 3, 4)
         x_a_masks = x_a_masks.reshape(b, n, -1, h, w).permute(0, 2, 1, 3, 4)
         x_v_masks = x_v_masks.reshape(b, n, -1, h, w).permute(0, 2, 1, 3, 4)
-        # x_a_masks = x_a_masks.reshape(b, n, -1, h, w)
-        # x_v_masks = x_v_masks.reshape(b, n, -1, h, w)
-        # final_seg = torch.stack([x_a_masks, x_v_masks], 2)  # (b, c, s, z, h, w)
+        x_d_masks = x_d_masks.reshape(b, n, -1, h, w).permute(0, 2, 1, 3, 4)
+        return x_p_masks, x_a_masks, x_v_masks, x_d_masks  # (b, c, z, h, w)
 
-        # if self.training:
-        #     return x_a_masks, final_seg
-        return x_a_masks, x_v_masks # (b, c, z, h, w)
-        # return x_a_masks, x_v_masks
 
-# from utils.ct_io import load_ct
-# import numpy as np
-# import cv2
-# import torchvision
-#
-# ct = load_ct("/data4/liver_CT3_Z2_ts4.0/ct_00055626.npz")
-# ct = torch.from_numpy(ct[2, 65 + 3]).float()
-# ct = np.clip(ct, -55, 155)
-# ct = (ct + 55) / (155 + 55)
-# # torchvision.utils.save_image(ct, "../log/img.png", nrow=9)
-# ct = ct.numpy()
-# feat_tmp = selected.reshape(b, n, 16, 32, 32)[0, 0+3]
-# ct_tmp = np.uint8(np.stack([ct, ct, ct], 2) * 255)
-# cv2.imwrite("../log/tokens/img-2-68.png", ct_tmp)
-# # ct_tmp = np.uint8(np.stack([ct_tmp, ct_tmp, ct_tmp], 2) * 255)
-# for i in range(16):
-#     heatmap = feat_tmp.cpu().numpy()[i]
-#     heatmap = (heatmap - np.min(heatmap)) / (np.max(heatmap) - np.min(heatmap))
-#     # heatmap = np.transpose(heatmap, (1, 2, 0))
-#     heatmap = cv2.resize(heatmap, (512, 512))
-#     # heatmap = np.transpose(heatmap, (2, 0, 1))
-#     # heatmap = np.concatenate(list(heatmap), 1)
-#     heatmap = np.uint8(255 * heatmap)
-#     heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-#     f_img = ct_tmp + heatmap * 0.4
-#     cv2.imwrite("../log/tokens/tokenmap-2-68-%d.png" % i, f_img)
-
-if __name__ == '__main__':
-    m = DualSeg().cuda()
-    a = torch.rand((10, 2, 3, 3, 256, 256)).cuda()
+# if __name__ == '__main__':
+#     m = DualSeg().cuda()
+#     a = torch.rand((10, 2, 3, 3, 256, 256)).cuda()
     # b = torch.rand((90, 8, 512)).cuda()
     # c = torch.rand((90, 8, 512)).cuda()
-    m(a)
+    # m(a)
     # m = TransformerBlock().cuda()
     # a = torch.rand((10, 2, 9, 512, 16, 16)).cuda()
     # m(a)
